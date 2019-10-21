eb82369e57f77c0f477693a09e2924439127a707
Akira <autumn.no7@gmail.com>
2019-10-21 18:57:34 +0900

    step4まで実装

--- shiritori_game.rb
+++ shiritori_game.rb
@@ -1,9 +1,38 @@
 class ShiritoriGame
+  attr_accessor :player_name, :histories
+
+  def initialize(player_name:)
+    @player_name = player_name
+    @histories = []
+  end
+
   def self.rule
     rule_text = ''
     rule_text << "すべてカタカナで入力してください\n"
     rule_text << "最後に 'ン' がついたら負けです\n"
     rule_text << "同じ単語を使ったら負けです\n"
     rule_text
+  end
+
+  def self.all_words
+    ["リンゴ", "ゴリラ", "ラッパ", "パリ"]
+  end
+
+  def start
+    "\\_o< Hi, #{@player_name}. Let's enjoy shiritori game\n"
+  end
+
+  def computer_attack
+    if self.class.all_words.include?(histories.last)
+      self.class.all_words.each do |word|
+        if word[0] == histories.last[-1]
+          return word
+        else
+          nil
+        end
+      end 
+    else
+      nil
+    end
   end
 end
--- spec/shiritori_game_spec.rb
+++ spec/shiritori_game_spec.rb
@@ -3,11 +3,47 @@ require './shiritori_game'
 RSpec.describe ShiritoriGame do
   describe '.rule' do
     it 'シリトリのルールが表示される' do
-      expect(ShiritoriGame.rule).to eq(puts(%Q|
-        すべてカタカナで入力してください
-        最後に 'ン' がついたら負けです
-        同じ単語を使ったら負けです
-      |))
+      expect(ShiritoriGame.rule).to eq((%Q|すべてカタカナで入力してください\n最後に 'ン' がついたら負けです\n同じ単語を使ったら負けです\n|))
     end
   end
+
+  describe '.all_words' do
+    it { expect(ShiritoriGame.all_words).to eq ["リンゴ", "ゴリラ", "ラッパ", "パリ"] }
+  end
+
+  describe '#start' do
+    subject(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+    it { expect(shiritori_game.start).to include("\\_o< Hi, シリトリプレイヤー. Let's enjoy shiritori game\n") }
+  end
+
+  describe '#computer_attack' do
+    subject(:shiritori_game_computer_attack) { shiritori_game.computer_attack }
+    let(:shiritori_game) { ShiritoriGame.new(player_name: 'シリトリプレイヤー') }
+
+    context 'ShiritoriGame#histories 最後が リンゴ のとき' do
+      before { shiritori_game.histories << 'リンゴ' }
+
+      it 'ゴリラが返ってくること' do
+        expect(shiritori_game_computer_attack).to eq('ゴリラ')
+      end
+    end
+
+    context 'ShiritoriGame#histories 最後が パリ のとき' do
+      before { shiritori_game.histories << 'パリ' }
+
+      it 'リンゴが返ってくること' do
+        expect(shiritori_game_computer_attack).to eq('リンゴ')
+      end
+    end
+
+    context 'ShiritoriGame#histories 最後が リンゴ, ゴリラ, ラッパ, パリ 以外のとき' do
+      before { shiritori_game.histories << 'イヌ' }
+
+      it 'nil が返ってくること' do
+        expect(shiritori_game_computer_attack).to eq(nil)
+      end
+    end
+  end
+
 end
+
